abb3eece0f1119b7b7780da336d1f5049581ab11
Franco <christopher.franco.m@gmail.com>
2018-01-27 07:23:21 -0800

    validation complete

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,5 +1,4 @@
 class ArticlesController < ApplicationController
-
   def index
     @articles = Article.all
   end
@@ -8,12 +7,16 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
   def new
+    @article = Article.new
   end
   def create
     @article = Article.new(article_params)
 
-    @article.save
-    redirect_to @article
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
   private
     def article_params
